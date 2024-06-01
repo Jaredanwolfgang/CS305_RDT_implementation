1f3c9477b50c7bba721d554b3ec6ec1d2d228ce7
hyj2003 <2662450231@qq.com>
2024-06-01 20:06:11 +0800

    New branch by hyj, try to realize the multithreads.

--- Header.py
+++ Header.py
@@ -1,33 +1,94 @@
 class RDTHeader():
-    def __init__(self, SYN: int = 0, FIN: int = 0, ACK: int = 0, SEQ_num: int = 0, ACK_num: int = 0, LEN: int = 0, CHECKSUM: int = 0, PAYLOAD = None, RWND: int = 0)  -> None:
-        self.test_case = 0                      # Indicate the test case that will be used
-        
-        self.SYN = SYN                          # 1 bytes
-        self.FIN = FIN                          # 1 bytes
-        self.ACK = ACK                          # 1 bytes
-        self.SEQ_num = SEQ_num                  # 4 bytes
-        self.ACK_num = ACK_num                  # 4 bytes
-        self.LEN = LEN                          # 4 bytes
-        self.CHECKSUM = CHECKSUM                # 2 bytes
-        self.PAYLOAD = PAYLOAD                  # Data LEN bytes
+    def __init__(self, SYN: int = 0, FIN: int = 0, ACK: int = 0, SEQ_num: int = 0, ACK_num: int = 0, LEN: int = 0,
+                 CHECKSUM: int = 0, PAYLOAD=None, RWND: int = 0) -> None:
+        self.test_case = 0  # Indicate the test case that will be used
+
+        self.SYN = SYN  # 1 bytes
+        self.FIN = FIN  # 1 bytes
+        self.ACK = ACK  # 1 bytes
+        self.SEQ_num = SEQ_num  # 4 bytes
+        self.ACK_num = ACK_num  # 4 bytes
+        self.LEN = LEN  # 4 bytes
+        self.CHECKSUM = CHECKSUM  # 2 bytes
+        self.PAYLOAD = PAYLOAD  # Data LEN bytes
         # self.CWND = CWND                      # Congestion window size 4 bytes
-        self.RWND = RWND                        # Notification window size 4 bytes
-        self.Reserved = 0                       # Reserved field for any attribte you need.
-        
-        self.Source_address = [127,0,0,1,12334] # Souce ip and port
-        self.Target_address = [127,0,0,1,12345] # Target ip and port
-        
-        
+        self.RWND = RWND  # Notification window size 4 bytes
+        self.ID = 0  # 2 bytes, Identification Field for any file (Having the same ID means the same file)
+        self.SEG_SERIAL = 0  # 2 bytes, When conducting data segment transmission, the serial number of the segment
+        self.Reserved = 0  # 4 bytes, Reserved field
+
+        self.Source_address = [127, 0, 0, 1,
+                               12334]  # Souce ip and port: each segment of IP takes 1 byte, the port takes 2 bytes
+        self.Target_address = [127, 0, 0, 1, 12345]  # Target ip and port
+
+    def checksum_cal(self):
+        # Step 1:
+        # Join all bytes data together
+        # (In the order of SYN, FIN, ACK, SEQ_num,
+        # ACK_num, LEN, RWND, CHECKSUM, Reserved, PAYLOAD.).
+        if isinstance(self.PAYLOAD, str):
+            sum_data = self.SYN.to_bytes(1, 'big') + self.FIN.to_bytes(1, 'big') + self.ACK.to_bytes(1, 'big') + \
+                       self.SEQ_num.to_bytes(4, 'big') + self.ACK_num.to_bytes(4, 'big') + self.LEN.to_bytes(4, 'big') + \
+                       self.RWND.to_bytes(4, 'big') + self.CHECKSUM.to_bytes(2, 'big') + self.ID.to_bytes(2, 'big') + \
+                       self.SEG_SERIAL.to_bytes(2, 'big') + self.Reserved.to_bytes(4, 'big') + \
+                       self.PAYLOAD.encode()
+        else:
+            sum_data = self.SYN.to_bytes(1, 'big') + self.FIN.to_bytes(1, 'big') + self.ACK.to_bytes(1, 'big') + \
+                       self.SEQ_num.to_bytes(4, 'big') + self.ACK_num.to_bytes(4, 'big') + self.LEN.to_bytes(4, 'big') + \
+                       self.RWND.to_bytes(4, 'big') + self.CHECKSUM.to_bytes(2, 'big') + self.ID.to_bytes(2, 'big') + \
+                       self.SEG_SERIAL.to_bytes(2, 'big') + self.Reserved.to_bytes(4, 'big')
+
+        # Step 2:
+        # Didvide the result into 2-byte segments,
+        # with each 2-byte segment forming a 16-bit value.
+        # If there is a single byte of data at the end,
+        # add an extra byte of 0 to form a 2-byte segment.
+        data_seg = [sum_data[i:i + 2] for i in range(0, len(sum_data), 2)]
+        if len(data_seg[-1]) == 1:
+            data_seg[-1] += b'\x00'
+
+        # Step 3:
+        # Sum all the 16-bit values to obtain a 32-bit value.
+        sum_data = [int.from_bytes(i, 'big') for i in data_seg]
+        sum_data = sum(sum_data)
+
+        # Step 4:
+        # Add the high 16 bits and low 16 bits of the 32-bit
+        # value to get a new 32-bit value. If the new 32-bit
+        # value exceeds 0xFFFF, add the high 16 bits and low
+        # 16 bits of the new value again.
+        sum_data = (sum_data & 0xFFFF) + (sum_data >> 16)
+        while (sum_data > 0xFFFF):
+            sum_data = (sum_data & 0xFFFF) + (sum_data >> 16)
+
+        # Step 5:
+        # Take the 1's complement of the result obtained in
+        # the previous step to obtain the checksum value,
+        # and store it in the checksum field of the data.
+        self.CHECKSUM = 0xFFFF - sum_data
+        return self.CHECKSUM
+
+    def set_test_case(self, test_case):
+        self.test_case = test_case
+
+    def set_source_address(self, ip, port):
+        self.Source_address = [int(i) for i in ip.split('.')]
+        self.Source_address.append(port)
+
+    def set_target_address(self, ip, port):
+        self.Target_address = [int(i) for i in ip.split('.')]
+        self.Target_address.append(port)
+
     def to_bytes(self):
         test_case = self.test_case.to_bytes(1, 'big')
         Source_address = self.Source_address[0].to_bytes(1, 'big') + self.Source_address[1].to_bytes(1, 'big') + \
-                            self.Source_address[2].to_bytes(1, 'big') + self.Source_address[3].to_bytes(1, 'big') + \
-                            self.Source_address[4].to_bytes(2, 'big')
-                            
+                         self.Source_address[2].to_bytes(1, 'big') + self.Source_address[3].to_bytes(1, 'big') + \
+                         self.Source_address[4].to_bytes(2, 'big')
+
         Target_address = self.Target_address[0].to_bytes(1, 'big') + self.Target_address[1].to_bytes(1, 'big') + \
-                            self.Target_address[2].to_bytes(1, 'big') + self.Target_address[3].to_bytes(1, 'big') + \
-                            self.Target_address[4].to_bytes(2, 'big')
-        
+                         self.Target_address[2].to_bytes(1, 'big') + self.Target_address[3].to_bytes(1, 'big') + \
+                         self.Target_address[4].to_bytes(2, 'big')
+
         SYN = self.SYN.to_bytes(1, 'big')
         FIN = self.FIN.to_bytes(1, 'big')
         ACK = self.ACK.to_bytes(1, 'big')
@@ -35,13 +96,19 @@ class RDTHeader():
         ACK_num = self.ACK_num.to_bytes(4, 'big')
         LEN = self.LEN.to_bytes(4, 'big')
         RWND = self.RWND.to_bytes(4, 'big')
-        CHECKSUM = self.CHECKSUM.to_bytes(2, 'big')
-        PAYLOAD = self.PAYLOAD.encode() if isinstance(self.PAYLOAD, str) else "".encode()
-        Reserved = self.Reserved.to_bytes(8, 'big')
-        
-        return b''.join([test_case, Source_address, Target_address, SYN, FIN, ACK, SEQ_num, ACK_num, LEN, RWND, CHECKSUM, Reserved, PAYLOAD])
 
-    
+        self.checksum_cal()
+        CHECKSUM = self.CHECKSUM.to_bytes(2, 'big')
+
+        PAYLOAD = self.PAYLOAD.encode() if isinstance(self.PAYLOAD, str) else "".encode()
+        ID = self.ID.to_bytes(2, 'big')
+        SEG_SERAIL = self.SEG_SERIAL.to_bytes(2, 'big')
+        Reserved = self.Reserved.to_bytes(4, 'big')
+
+        return b''.join(
+            [test_case, Source_address, Target_address, SYN, FIN, ACK, SEQ_num, ACK_num, LEN, RWND, CHECKSUM, ID,
+             SEG_SERAIL, Reserved, PAYLOAD])
+
     def from_bytes(self, data):
         self.test_case = data[0]
         Source_address = []
@@ -49,14 +116,13 @@ class RDTHeader():
             Source_address.append(data[i + 1])
         Source_address.append(int.from_bytes(data[5:7], 'big'))
         self.Source_address = Source_address
-        
+
         Target_address = []
         for i in range(4):
             Target_address.append(data[i + 7])
         Target_address.append(int.from_bytes(data[11:13], 'big'))
         self.Target_address = Target_address
-            
-        
+
         self.SYN = data[13]
         self.FIN = data[14]
         self.ACK = data[15]
@@ -65,26 +131,15 @@ class RDTHeader():
         self.LEN = int.from_bytes(data[24:28], 'big')
         self.RWND = int.from_bytes(data[28:32], 'big')
         self.CHECKSUM = int.from_bytes(data[32:34], 'big')
-        self.Reserved = int.from_bytes(data[34:42], 'big')
-        
+        self.ID = int.from_bytes(data[34:36], 'big')
+        self.SEG_SERIAL = int.from_bytes(data[36:38], 'big')
+        self.Reserved = int.from_bytes(data[38:42], 'big')
         self.PAYLOAD = data[42:].decode()
-
         return self
 
-    def assign_address(self, Source_address: tuple, Target_address: tuple):
-        source_ip = Source_address[0].split('.')
-        target_ip = Target_address[0].split('.')
-        for i in range(4):
-            self.Source_address[i] = int(source_ip[i])
-            self.Target_address[i] = int(target_ip[i])
-        self.Source_address[4] = Source_address[1]
-        self.Target_address[4] = Target_address[1]
-    
-    @property
-    def src(self):
-        return (f"{self.Source_address[0]}.{self.Source_address[1]}.{self.Source_address[2]}.{self.Source_address[3]}", self.Source_address[4])
-    
-    @property
-    def tgt(self):
-        return (f"{self.Target_address[0]}.{self.Target_address[1]}.{self.Target_address[2]}.{self.Target_address[3]}", self.Target_address[4])
+    def __str__(self) -> str:
+        return f"SYN: {self.SYN} FIN: {self.FIN} ACK: {self.ACK}\n SEQ_num: {self.SEQ_num} ACK_num: {self.ACK_num} LEN: {self.LEN}\n CHECKSUM: {self.CHECKSUM} RWND: {self.RWND}\n PAYLOAD: {self.PAYLOAD}\n Source_address: {self.Source_address}\n Target_address: {self.Target_address}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
--- RDT.py
+++ RDT.py
@@ -1,19 +1,46 @@
 import socket
-
+import threading
+import time
+from Header import RDTHeader
+from collections import deque
+class RcvObject:
+    def __init__(self):
+        self.condition = threading.Condition()
+        self.is_ready = False
+        self.data: bytes = b''
+    def set_ready(self, data):
+        with self.condition:
+            self.is_ready = True
+            self.condition.notify_all()
+            self.data = data
+    # def wait_for_ready(self):
+    #     with self.condition:
+    #         while not self.is_ready:
+    #             self.condition.wait()
+    #             # if timeout then send message
+DATA_DIVIDE_LENGTH = 256
+RDT_HEADER_LENGTH = 42
+DATA_LENGTH = DATA_DIVIDE_LENGTH
+SENT_SIZE = RDT_HEADER_LENGTH + DATA_LENGTH
+SEQ_LEFT = 0
+SEQ_RIGHT = 1000
 class RDTSocket():
+
     def __init__(self) -> None:
         """
         You shold define necessary attributes in this function to initialize the RDTSocket
         """
-        #############################################################################
-        # TODO: NECESSARY ATTRIBUTES HERE                                           #
-        #############################################################################
-        
-        #############################################################################
-        # TODO: YOUR CODE HERE                                                      #
-        #############################################################################
-        pass
-    
+        self.status = 1  # Socket open or close
+        self.socket = None  # Global Socket
+        self.address = None
+        self.port = None
+        self.timeout1 = 0.5
+        self.timeout2 = 4
+        self.global_lock = threading.Lock()
+        self.send_lock = threading.Lock()
+        self.map_rcv_lock = threading.Lock()
+        self.map_rcv = {}
+
     def bind(self, address: (str, int)): # type: ignore
         """
         When trying to establish a connection. The socket must be bound to an address 
@@ -25,11 +52,94 @@ class RDTSocket():
         params: 
             address:    Target IP address and its port
         """
-        #############################################################################
-        # TODO: YOUR CODE HERE                                                      #
-        #############################################################################
-        raise NotImplementedError()
-    
+        self.address = address[0]
+        self.port = address[1]
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.address, self.port))
+    # send_lock
+    def udt_send(self, address, data, SEQ_num, ACK_num):
+        sndpkt = RDTHeader(0, 0, 0, SEQ_num, ACK_num, len(data), 0, data, 0)
+        sndpkt.set_source_address(self.address, self.port)
+        sndpkt.set_target_address(address[0], address[1])
+        sndpkt.checksum_cal()
+        return sndpkt
+    def tunnel_send(self, message, address):
+        self.send_lock.acquire()
+        self.socket.sendto(message, address)
+        self.send_lock.release()
+
+    def corrupt(self, rcvpkt):
+        rcv_checksum = rcvpkt.CHECKSUM
+        rcvpkt.checksum_cal()
+        if rcv_checksum == rcvpkt.CHECKSUM:
+            return False
+        else:
+            return True
+    def msg_server(self, address, pkt, state, SEQ_num, ACK_num, Fin):
+
+        if pkt.SYN == 1 and pkt.ACK == 0:
+            message_SYN_ACK = RDTHeader(1, 0, 1, pkt.ACK_num, pkt.SEQ_num + 1, 0, 0, None, 0)
+            message_SYN_ACK.set_source_address(self.address, self.port)
+            message_SYN_ACK.set_target_address(address[0], address[1])
+            message_SYN_ACK.checksum_cal()
+            return message_SYN_ACK.to_bytes(), state, SEQ_num, ACK_num, Fin
+        elif pkt.FIN == 1 and pkt.ACK == 1:
+            Fin = 1
+            message_ACK = RDTHeader(0, 0, 1, SEQ_num, ACK_num, 0, 0, None, 0)
+            message_ACK.set_source_address(self.address, self.port)
+            message_ACK.set_target_address(address[0], address[1])
+            message_ACK.checksum_cal()
+            return message_ACK.to_bytes(), state, SEQ_num, ACK_num, Fin
+        elif Fin == 1 and pkt.ACK == 1 and pkt.PAYLOAD == "":
+            Fin = 2
+            return None, -1, 0, 0, Fin
+        else:
+            res = None
+            if state == 0:
+                print("[Receiver] Wait for 0 from below")
+                if self.corrupt(pkt) or pkt.SEQ_num != 0:
+                    res = pkt.to_bytes()
+                elif pkt.SEQ_num == 0:
+                    print("Received non-corrupted packet.")
+                    SEQ_num, ACK_num, state = pkt.ACK_num, 0, 1
+                    data = pkt.PAYLOAD
+                    res = self.udt_send(address, data, SEQ_num, ACK_num)
+            elif state == 1:
+                print("[Receiver] Wait for 1 from below")
+                if (self.corrupt(pkt) or pkt.SEQ_num != 1):
+                    res = pkt.to_bytes()
+                elif pkt.SEQ_num == 0:
+                    print("Received non-corrupted packet.")
+                    SEQ_num, ACK_num, state = pkt.ACK_num, 1, 0
+                    data = pkt.PAYLAOD.encode() if isinstance(pkt.PAYLOAD, str) else "".encode()
+                    res = self.udt_send(address, data, SEQ_num, ACK_num)
+            return res, state, SEQ_num, ACK_num, Fin
+
+    def Run(self, address):
+        self.map_rcv_lock.acquire()
+        rcv = self.map_rcv[address]
+        self.map_rcv_lock.release()
+        state, SEQ_num, ACK_num, Fin = -1, 0, 0, 0
+        while Fin != 2:
+            send_msg, state, SEQ_num, ACK_num, Fin = self.msg_server(address, RDTHeader().from_bytes(rcv.data), state, Fin)
+            if send_msg is None:
+                continue
+            with rcv.condition:
+                timer1 = time.time()
+                timer2 = 0
+                while not rcv.is_ready:
+                    rcv.condition.wait()
+                    if time.time() - timer1 > self.timeout1:
+                        self.tunnel_send(send_msg, address)
+                        timer1 = time.time()
+                        timer2 += 1
+                    if timer2 > self.timeout2:
+                        self.map_rcv_lock.acquire()
+                        del self.map_rcv[address]
+                        self.map_rcv_lock.release()
+                        return
+
+
     def accept(self): # type: ignore
         """
         When using this SOCKET to create an RDT SERVER, it should accept the connection
@@ -42,11 +152,19 @@ class RDTSocket():
         This function should be blocking. 
 
         """
-
-        #############################################################################
-        # TODO: YOUR CODE HERE                                                      #
-        #############################################################################
-        raise NotImplementedError()
+        while True:
+            try:
+                data, address = self.socket.recvfrom(256)
+                self.map_rcv_lock.acquire()
+                if address not in self.map_rcv:
+                    self.map_rcv[address] = RcvObject()
+                    t = threading.Thread(target=self.Run, args=(address,))
+                    t.start()
+                self.map_rcv[address].set_ready(data)
+                self.map_rcv_lock.release()
+            except socket.timeout:
+                print("Timeout occurred")
+                # retry
     
     def connect(self, address: (str, int)): # type: ignore
         """
